8078bf95bac4712ff42bb5ab25932364cb453a37
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-06-26 21:56:45 +0100

    added tailwind css and vue js

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 new Vue({
     el: '#main',
     data: {
-      message: 'Hello Vue.js2'
+      errors: [],
+      first: null,
+      last: null,
+      pass: null,
+      firm: null
+      
+    },
+
+    methods: {
+        checkForm: function(e) {
+            if(this.first && this.last && this.pass && this.firm) {
+                return true
+            }
+            this.errors = [];
+
+            if(!this.first){
+                this.errors.push('please enter your first name')
+            }
+            if(!this.age){
+                this.errors.push('please enter your last name')
+            }
+
+            if(!this.pass){
+                this.errors.push('please enter your password')
+            }
+            if(!this.pass){
+                this.errors.push('please enter confirm your password')
+            }
+            e.preventDefault();
+            
+        }
+
+
+
+
     }
   })
